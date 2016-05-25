014886ab6db2fdfbf85bf57f67a12ace981dc2ec
Yifan Wang <namelos@outlook.com>
2016-05-25 22:14:19 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using static System.Console;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, Func<int, Func<int, int>>> add = x => y => z => x + y + z;
+
+            WriteLine(add(1)(2)(3));
+
+            Func<Func<int>> Closure = () =>
+            {
+                int property = 1;
+                return () =>
+                {
+                    WriteLine(property);
+                    return property = property + 1;
+                };
+            };
+
+            var closure = Closure();
+
+            closure();
+            closure();
+            closure();
+
+            ReadLine();
+        }
+    }
+}
